677bc268ac7dcb329fb381e9cc0f1b7d9527106d
Quozul <erwan.legloannec@dartybox.com>
2018-08-09 14:01:33 +0200

    Updated buttons
    Updated skill tree buttons
    Updated crate smash sound

--- clib/buttons.lua
+++ clib/buttons.lua
@@ -1,63 +1,151 @@
 local button = {}
 button.__index = button
-local sould_occur = true
+local anim = .5
 
-function NewButton(type, x, y, w, h, clic, t, value) -- t should be {text, color r, color g, color b, font} -- clic is a function -- value is for checkboxes
-    -- type can be "checkbox" or "button"
+tx, ty = 0, 0
+
+width, height = love.window.getMode()
+
+function NewButton(type, x, y, w, h, click, text, color, style, value)
+    if style == nil then style = {} end
+
     local b = {}
     b.type = type
-    b.value = value
-    if type == "checkbox" then b.checked = value end
     b.x, b.y = x, y
     b.w, b.h = w, h
-    b.text, b.r, b.g, b.b, b.font = unpack(t)
-    b.clic = clic
+    b.click = click
+    b.defaultText = text or ""
+    b.text = b.defaultText
+    if type == "key input" then
+        b.text = b.defaultText .. ": " .. value
+    end
+    b.r, b.g, b.b = unpack(color)
+    b.timeIn = 0
+    b.style = style
+    b.alpha = .5
+    b.trans = NewTransition(0, 0, 0, style.easing)
+    b.isDown = false
+    b.border = 1
+    b.value = value
 
     return setmetatable(b, button)
 end
 
-function button:draw() -- also act as update
-    if self.type == "button" then
-        love.graphics.setFont(self.font)
+function SetSounds(hover, click)
+    hover_sound = hover
+    click_sound = click
+end
 
-        if between(love.mouse.getX(), self.x, self.x + self.w) and between(love.mouse.getY(), self.y, self.y + self.h) then
-            love.graphics.setColor(self.r - .25, self.g - .25, self.b - .25)
-        else
-            love.graphics.setColor(self.r, self.g, self.b)
+function SetTranslation(x, y)
+    tx, ty = x or 0, y or 0
+end
+
+function button:isIn()
+    return between(love.mouse.getX() - tx, self.x, self.x + self.w) and between(love.mouse.getY() - ty, self.y, self.y + self.h)
+end
+
+function button:update(dt)
+    if self:isIn() and self.trans:isDone() and self.alpha < anim then
+        if self.style.easing then
+            self.alpha = anim
+            self.trans = NewTransition(0, self.w, anim, self.style.easing)
+        
+            hover_sound:stop()
+            hover_sound:play()
         end
-        rwrc("fill", self.x, self.y, self.w, self.h, 8)
-
-        love.graphics.setColor(0, 0, 0)
-        love.graphics.print(self.text, self.x + self.w / 2 - self.font:getWidth(self.text) / 2,  self.y + self.h / 2 - self.font:getHeight(self.text) / 2)
-    elseif self.type == "checkbox" then
-        love.graphics.setFont(self.font)
-
-        love.graphics.setColor(1, 1, 1)
-        if not self.checked then
-            love.graphics.rectangle("line", self.x, self.y, self.w, self.h)
-        elseif self.checked then
-            love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)
-        end
-
-        love.graphics.setColor(1, 1, 1)
-        love.graphics.print(self.text, self.x + self.w + 10, self.y)
+    elseif not self:isIn() and self.trans and self.trans:isDone() then
+        self.alpha = self.alpha - dt
+    end
+    
+    if self.trans then
+        self.trans:update(dt)
+    end
+    
+    if self.isDown then
+        self.border = math.min(self.border + dt * 2, 2)
+    else
+        self.border = math.max(self.border - dt, 1)
     end
 end
 
-function button:mouse()
-    if self.type == "button" then
-        if between(love.mouse.getX(), self.x, self.x + self.w) and between(love.mouse.getY(), self.y, self.y + self.h) then
-            if love.mouse.isDown(1) then
-                self.clic()
+function button:draw() -- also act as update
+    local font = love.graphics.getFont()
+
+    --[[local shape = {
+        self.x + 5, self.y + 5,
+        self.x + 5, self.y,
+        self.x + self.w - 5, self.y,
+        self.x + self.w - 5, self.y + 5,
+        self.x + self.w, self.y + 5,
+        self.x + self.w, self.y + self.h - 5,
+        self.x + self.w - 5, self.y + self.h - 5,
+        self.x + self.w - 5, self.y + self.h,
+        self.x + 5, self.y + self.h,
+        self.x + 5, self.y + self.h - 5,
+        self.x, self.y + self.h - 5,
+        self.x, self.y + 5
+    }]]
+    
+    love.graphics.setColor(self.r / 255, self.g / 255, self.b / 255, 1)
+       
+    love.graphics.rectangle("fill", self.x, self.y, self.w, self.h, 5)
+    --love.graphics.polygon("fill", unpack(shape))
+    
+    local time = self.trans:GetProgress()
+        
+    if time ~= 0 then
+        love.graphics.setColor(self.r / 255 - self.alpha / 4, self.g / 255 - self.alpha / 4, self.b / 255 - self.alpha / 4, self.alpha)
+        
+        if self.style.shape == "sharp" then
+            --love.graphics.polygon("fill",
+            --    self.x, self.y + self.h,
+            --    self.x, self.y,
+            --    self.x + math.min(time + math.min(time, self.h / 2), self.w), self.y,
+            --    self.x + math.min(time, self.w), self.y + self.h
+            --)
+            love.graphics.rectangle("fill", self.x, self.y, time, self.h, 5)
+        else
+            love.graphics.rectangle("fill", self.x, self.y, time, self.h)
+        end
+    end
+    
+    love.graphics.setColor(0, 0, 0, 1)
+    love.graphics.setLineWidth(self.border)
+    --love.graphics.polygon("line", unpack(shape))
+    love.graphics.rectangle("line", self.x, self.y, self.w, self.h, 5)
+
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.print(self.text, self.x + self.w / 2 - font:getWidth(self.text) / 2,  self.y + self.h / 2 - font:getHeight(self.text) / 2)
+end
+
+function button:mousepressed(x, y, button)
+    if button == 1 and self.type ~= "group" then
+        if self:isIn() then
+            click_sound:play()
+            self.isDown = true
+            
+            if self.type == "key input" then
+                self.text = "..."
             end
         end
-    elseif self.type == "checkbox" then
-        if between(love.mouse.getX(), self.x, self.x + self.w) and between(love.mouse.getY(), self.y, self.y + self.h) then
-            if love.mouse.isDown(1) then
-                self.checked = not self.checked
-                self.clic()
-            end
+    end
+end
+
+function button:mousereleased(x, y, button)
+    if button == 1 and self.type == "button" then
+        self.isDown = false
+        if self:isIn() then
+            self.click()
         end
+    end
+end
+
+function button:keypressed( key, scancode, isrepeat )
+    if self.type == "key input" and self.isDown then
+        self.isDown = false
+        self.value = key
+        self.text = self.defaultText .. ": " .. self.value
+        self.click(key)
     end
 end
 
--- clib/transition.lua
+++ clib/transition.lua
@@ -1,26 +1,61 @@
-local transitions = {}
+local trans = {}
+trans.__index = trans
 
-function NewTransition(name, start_point, end_point, duration)
-    local t = {}
-    t.current_point = start_point
-    t.end_point = end_point
-    t.time_elapsed = 0
-    t.duration = duration
-
-    table.insert(t, transitions, name)
+local function linear(current_time, start_point, change, duration)
+    return change * current_time / duration + start_point
 end
 
-function UpdateTransitions(dt)
-    for name, trans in pairs(transitions) do
-        math.min(trans.current_point + dt, trans.end_point)
-        trans.time_elapsed = trans.time_elapsed + dt
+local function easeIn(current_time, start_point, change, duration) -- accelerating from zero velocity 
+    return change * math.pow(2, 10 * (current_time / duration - 1) + start_point)
+end
 
-        if trans.time_elapsed >= duration then
-            table.remove(transitions, name)
-        end
+local function easeOut(current_time, start_point, change, duration) -- decelerating to zero velocity
+    return change * (-math.pow(2, -10 * current_time / duration) + 1) + start_point
+end
+
+local function easeInOut(current_time, start_point, change, duration) -- accelerating until halfway, then decelerating 
+    current_time = current_time / (duration / 2)
+    if current_time < 1 then return change / 2 * math.pow(2, 10 * (current_time - 1)) + start_point end
+    current_time = current_time - 1
+    return change / 2 * (-math.pow(2, -10 * current_time) + 2) + start_point
+end
+
+function NewTransition(start_point, end_point, duration, type)
+    local t = {}
+    t.type = type or "linear"
+    t.current_point = start_point
+    t.start_point = start_point
+    t.end_point = end_point
+    t.time_elapsed = 0
+    t.duration = duration or 1
+    t.done = false
+
+    return setmetatable(t, trans)
+end
+
+function trans:update(dt)
+    local values = {self.time_elapsed, self.start_point, self.end_point - self.start_point, self.duration}
+    
+    if self.type == "inOut" then
+        self.current_point = math.min(easeInOut(unpack(values)), self.end_point)
+    elseif self.type == "in" then
+        self.current_point = math.min(easeIn(unpack(values)), self.end_point)
+    elseif self.type == "out" then
+        self.current_point = math.min(easeOut(unpack(values)), self.end_point)
+    else
+        self.current_point = math.min(linear(unpack(values)), self.end_point)
+    end
+    
+    self.time_elapsed = self.time_elapsed + dt
+    
+    if self.time_elapsed >= self.duration then
+        self.done = true
+        return
     end
 end
 
-function GetProgress(name)
-    return transitions[name].current_point
+function trans:GetProgress()
+    return self.current_point, self.time_elapsed
 end
+
+function trans:isDone() return self.done end
