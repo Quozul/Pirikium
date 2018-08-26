47183f35a025bb11b288cb0f79f234199376eabb
Quozul <30729291+Quozul@users.noreply.github.com>
2018-08-26 11:43:59 +0200

    Fixes
    Fixed scale on entering game scene
    Fixed enemies tags

--- clib/utility.lua
+++ clib/utility.lua
@@ -2,6 +2,7 @@ function between(value, min, max) return value >= min and value <= max end
 function signe(num) if num >= 0 then return 1 elseif num < 0 then return 0 end end
 function sl(x1, y1, x2, y2) return math.sqrt( (x2 - x1)^2 + (y2 - y1)^2 ) end -- segment lengh
 function round(num, decimals) return math.floor(num * 10^(decimals or 0) + 0.5) / 10^(decimals or 0) end
+function removeDecimal(num) num = tostring(num) return tonumber(num:split(".")[1]) end -- remove decimals
 function rf(min, max, decimals) return math.random(min * 10^decimals, max * 10^decimals) / 10^decimals end -- random float
 function percent(chance) return math.random(0, 100) <= chance end -- return true if value < chance
 function isIn(value, table) for index, element in pairs(table) do if value == element then return true end end end -- check if the given value is in a table
